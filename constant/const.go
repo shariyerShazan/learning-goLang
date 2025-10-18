608dc063059a949de99d664281dd1371e1a37ea3
shariyerShazan <shariyershazan1@gmail.com>
2025-10-18 10:37:14 +0600

    const

--- constant/const.go
+++ constant/const.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const fullName = "shariyer shazan" // we can declear const outside of main function
+var age2 = 22 // we can declear var outside of main function
+
+// text := "sms" //! we can't declear shortform or declearation outside of the main function
+
 func main(){
 	var name = "shazan" 
 	fmt.Println(name)
@@ -14,4 +19,7 @@ func main(){
 	const age = 20
 	// age = 200 //! its will error
 	fmt.Println(age)
+
+	fmt.Println(fullName)
+	fmt.Println(age2)
 }
