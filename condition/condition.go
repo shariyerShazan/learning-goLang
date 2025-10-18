1af1229053826cfe419446d800dc1581a4cad6f2
shariyerShazan <shariyershazan1@gmail.com>
2025-10-18 10:56:35 +0600

    condition on goLang, if-elseIf-else

--- condition/condition.go
+++ condition/condition.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main (){
-	
+	age := 20
+
+	if age > 18 {
+		fmt.Println("You are adult!")
+	} else if age > 12{
+		fmt.Println("You are teenager")
+	} else if age > 0 {
+		fmt.Println("You are child")
+	}else {
+		fmt.Println("Not a valid age!")
+	}
 }
