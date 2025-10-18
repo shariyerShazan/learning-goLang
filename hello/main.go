91ae9f846d71b3f5c4a432cf198c278c95cc1c89
shariyerShazan <shariyershazan1@gmail.com>
2025-10-18 10:09:17 +0600

    go data types

--- hello/main.go
+++ hello/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("hello, I'm Shazan")
+
+	// string
+	fmt.Println("hello, I'm Shazan");
+
+	// int
+	fmt.Println(25);
+
+	// float
+	fmt.Println(25.08);
+
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+   //  action
+   fmt.Println(10/2)
 }
