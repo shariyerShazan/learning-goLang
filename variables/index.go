270c85ba349d6ff44cb7227781373ebf2ecafbfe
shariyerShazan <shariyershazan1@gmail.com>
2025-10-18 10:15:13 +0600

    variable and shortcut to declear variables

---
+++ variables/index.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	// type defined
+	var name string = "shazan";
+	fmt.Println(name);
+
+	// no need to define type & auto infer type
+   var keyword = "shariyer";
+   fmt.Println(keyword);
+
+
+   //  int value
+   var age int = 22;
+   fmt.Println(age);
+
+
+	//short way to declear variable.
+	fullName := "Shariyer Shazan" ;
+	fmt.Println(fullName);
+}
