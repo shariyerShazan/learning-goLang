ab7cbaa4ba2077ab4094d3ddca9772970951bc61
shariyerShazan <shariyershazan1@gmail.com>
2025-10-18 10:48:44 +0600

    range in loop

--- forLoop/for.go
+++ forLoop/for.go
@@ -5,6 +5,7 @@ import "fmt"
 //  for is one and only for looping in goLang. no while or do while
 func main(){
 	// ! for loop 
+	fmt.Println("Foor loop")
 	 for i := 0; i <= 10 ; i++ {
 		 fmt.Println(i)
 	 }
@@ -16,11 +17,20 @@ func main(){
 
 
 	// ! like while loop but writen by for keyword
-     j := 1
-	 for j < 10 {
+	fmt.Println("While but written like for")
+     j := 0
+	 for j <= 10 {
+		// ?  break  // for exit from the loop
+		// ? continue // for exit for the current value of index
+
 		fmt.Println(j)
 		j++
 	 }
-
+ 
+	// ! range looop
+	fmt.Println("Rnage loop")
+	for x:= range 10 {
+		fmt.Println(x)
+	}
 
 }
