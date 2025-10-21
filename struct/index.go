6c07b2232cfaa3b1e08e7da36818149ae9c9d7f5
shariyerShazan <shariyershazan1@gmail.com>
2025-10-22 03:18:01 +0600

    order status and amount chn age with func and pointer

--- struct/index.go
+++ struct/index.go
@@ -11,15 +11,33 @@ type order struct {
 
 }
 
+func (o *order) changeStatus(status string){
+	o.status = status
+	// *o.status = status 
+	// we need not to use pointer in stuct .. because stuct by default do that
+}
+
+func (o *order) changeAmount(amount float32){
+	o.amount = amount
+}
 
 func main() {
 	fmt.Println("struct")
 	myOrder := order{
 		id: 1,
 		amount: 50.20,
-		status: "Complete",
+		status: "pending",
 	}
 	myOrder.createdAt = time.Now()
 
 	fmt.Println("Order struct =" , myOrder) // output is -> Order struct = {1 50.2 Complete {13994398592634776448 286835 0x104f626e0}}
+
+	// after change status with func and pointer..
+	myOrder.changeStatus("confirmed")
+	fmt.Println("Order struct after change the status =" , myOrder) // output -> Order struct after change the status = {1 50.2 confirmed {13994403584273761224 242584 0x100cea6e0}}
+	
+
+	// afteer change amount with func and pointer
+	myOrder.changeAmount(4550)
+	fmt.Println("Order struct after change the amount =" , myOrder) // output -> Order struct after change the amount = {1 4550 confirmed {13994403584273761224 242584 0x100cea6e0}}
 }
