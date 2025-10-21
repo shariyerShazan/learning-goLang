e67aa91e1baa4ca68fad06ec8f04d0a3441c0d28
shariyerShazan <shariyershazan1@gmail.com>
2025-10-21 12:41:33 +0600

    range

--- range/index.go
+++ range/index.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main(){
-	
+
+	// integer array
+	 nums := []int{1,5,10}
+	 sum := 0
+	 for index , num := range nums {
+		sum += num
+		fmt.Println(index , "No of value is =" , num)
+	 }
+	 fmt.Println( "Total sum = ", sum)
+
+
+
+	//  string array
+	lifeStatus := map[string]string{"name" : "shazan" , "wife" : "priya"}
+	for key , value := range lifeStatus {
+		fmt.Println(key , value)
+	}
 }
