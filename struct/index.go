823c98637b43a243dd481dd79e7d866a93ce0d87
shariyerShazan <shariyershazan1@gmail.com>
2025-10-22 03:45:16 +0600

    interfaces-

--- struct/index.go
+++ struct/index.go
@@ -21,6 +21,10 @@ func (o *order) changeAmount(amount float32){
 	o.amount = amount
 }
 
+func (o order) getId()int  {
+	return o.id
+}
+
 func main() {
 	fmt.Println("struct")
 	myOrder := order{
@@ -35,9 +39,18 @@ func main() {
 	// after change status with func and pointer..
 	myOrder.changeStatus("confirmed")
 	fmt.Println("Order struct after change the status =" , myOrder) // output -> Order struct after change the status = {1 50.2 confirmed {13994403584273761224 242584 0x100cea6e0}}
-	
+
 
 	// afteer change amount with func and pointer
 	myOrder.changeAmount(4550)
 	fmt.Println("Order struct after change the amount =" , myOrder) // output -> Order struct after change the amount = {1 4550 confirmed {13994403584273761224 242584 0x100cea6e0}}
+
+
+	fmt.Println("myOrder id is =" , myOrder.getId()) // output -> 1
+
+
+
+	// if you dont set any field, default value is zero value  
+	
+	//int default -> 0 , float default -> 0 , string default -> "" , bool default -> false
 }
