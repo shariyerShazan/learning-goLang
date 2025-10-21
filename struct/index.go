9e247b25ae0d000526d7cd0fc5c2887e0c275373
shariyerShazan <shariyershazan1@gmail.com>
2025-10-22 02:00:16 +0600

    struct

--- struct/index.go
+++ struct/index.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import "time"
+import "fmt" 
+
+type order struct {
+	id int 
+	amount float32
+	status string
+	createdAt time.Time
+
+}
+
 
 func main() {
 	fmt.Println("struct")
-	//
+	myOrder := order{
+		id: 1,
+		amount: 50.20,
+		status: "Complete",
+	}
+	myOrder.createdAt = time.Now()
+
+	fmt.Println("Order struct =" , myOrder) // output is -> Order struct = {1 50.2 Complete {13994398592634776448 286835 0x104f626e0}}
 }
